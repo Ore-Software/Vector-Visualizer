d16ce3a4fafc5031f73f7f39519bce0d9c5069e0
Jason Fong <jasonlmfong@gmail.com>
2023-03-05 13:47:36 -0500

    added x,y,z axes lines rendering

--- Vector-Visualizer/src/main.cpp
+++ Vector-Visualizer/src/main.cpp
@@ -61,16 +61,22 @@ void main()
     unsigned int vb;
     glGenBuffers(1, &vb);
     glBindBuffer(GL_ARRAY_BUFFER, vb);
-    static const float positions[6]
+    static const float positions[6 * 4]
     {
-       -0.5f, -0.5f, 0.0f,
+       -0.5f, -0.5f, 0.0f, // vector
         1.0f,  1.0f, 0.0f,
+        0.0f,  0.0f, 0.0f, // x axis
+        1.0f,  0.0f, 0.0f,
+        0.0f,  0.0f, 0.0f, // y axis
+        0.0f,  1.0f, 0.0f,
+        0.0f,  0.0f, 0.0f, // z axis
+        0.0f,  0.0f, 1.0f,
     };
     glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 
     GLint size = 0;
     glGetBufferParameteriv(GL_ARRAY_BUFFER, GL_BUFFER_SIZE, &size);
-    if (sizeof(float) * 6 != size)
+    if (sizeof(float) * 6 * 4 != size)
     {
         glDeleteBuffers(1, &vb);
         std::cout << "buffer error" << std::endl;
@@ -143,7 +149,7 @@ void main()
         /* Render here */
         glBindVertexArray(va);
         glBindBuffer(GL_ARRAY_BUFFER, vb);
-        glDrawArrays(GL_LINES, 0, 2);
+        glDrawArrays(GL_LINES, 0, 2 * 4); // draw 4 lines
         //glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
