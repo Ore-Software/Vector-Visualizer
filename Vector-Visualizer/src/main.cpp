12ff87b1112ff06a7fef86f8d9c03b17e8708070
Jason Fong <jasonlmfong@gmail.com>
2023-03-24 21:20:15 -0400

    added movement and rotation (bugged)

--- Vector-Visualizer/src/main.cpp
+++ Vector-Visualizer/src/main.cpp
@@ -36,8 +36,8 @@ int main()
     // vector setup
     static const float vector[]
     {
-        0.0f, 0.0f, 0.0f,
-        20.0f,  5.0f, 0.0f,
+        0.0f, 0.0f, 0.0f,    // origin
+        20.0f,  5.0f, 0.0f,  // endpoint
     };
 
     unsigned int vectorVA;
@@ -60,18 +60,17 @@ int main()
 
     // camera
     glm::vec3 cameraPosition = { 20.0f, 20.0f, 20.0f };
-    glm::vec3 targetPosition = { 0.0f, 0.0f, 0.0f };
+    glm::vec3 direction = { 10.0f, 10.0f, 20.0f };
 
-    glm::vec3 direction = cameraPosition - targetPosition;
-    glm::normalize(direction);
+    glm::vec3 unitDirection = glm::normalize(direction);
 
     glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-    glm::vec3 cameraRight = glm::normalize(glm::cross(up, direction));
-    glm::vec3 cameraUp = glm::cross(direction, cameraRight);
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, unitDirection));
+    glm::vec3 cameraUp = glm::cross(unitDirection, cameraRight);
 
     glm::mat4 modelMatrix = glm::mat4(1.0f);
-    glm::mat4 viewMatrix = glm::lookAt(cameraPosition, targetPosition, cameraUp);
-    glm::mat4 projMatrix = glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 100.0f);
+    glm::mat4 viewMatrix = glm::lookAt(cameraPosition, cameraPosition - direction, cameraUp);
+    glm::mat4 projMatrix = glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 1000.0f);
 
     glm::mat4 MVP = projMatrix * viewMatrix * modelMatrix;
 
@@ -85,9 +84,151 @@ int main()
     glEnable(GL_DEPTH_TEST);
     glLineWidth(5.0f);
 
+    GLFWwindow* windowID = window.GetID();
+
+    double currentTime = 0.0;
+    double lastTime = 0.0;
+    float deltaTime = 0.0;
+    float forwardSpeed = 5.0f;
+    float strafeSpeed = 50.0;
+
+    double lastXpos, lastYpos, currXpos, currYpos, deltaX, deltaY;
+    glfwGetCursorPos(windowID, &currXpos, &currYpos);
+    double yaw = 0.0;
+    double pitch = 0.0;
+    double sens = 1.0/720.0;
+
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window.GetID()))
+    while (!glfwWindowShouldClose(windowID))
     {
+        lastTime = currentTime;
+        currentTime = glfwGetTime();
+        deltaTime = float(currentTime - lastTime);
+
+        // camera
+        glm::vec3 unitDirection = glm::normalize(direction);
+
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+        glm::vec3 cameraRight = glm::normalize(glm::cross(up, unitDirection));
+        glm::vec3 cameraUp = glm::cross(unitDirection, cameraRight);
+
+        glm::mat4 modelMatrix = glm::mat4(1.0f);
+        glm::mat4 viewMatrix = glm::lookAt(cameraPosition, cameraPosition - direction, cameraUp);
+        glm::mat4 projMatrix = glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 1000.0f);
+
+        glm::mat4 MVP = projMatrix * viewMatrix * modelMatrix;
+
+        // upload uniforms
+        axesShader.Bind();
+        axesShader.SetUniformMat4f("u_MVP", MVP);
+        vectorShader.Bind();
+        vectorShader.SetUniformMat4f("u_MVP", MVP);
+
+        // keyboard input
+        // Move forward
+        if (glfwGetKey(windowID, GLFW_KEY_W) == GLFW_PRESS) 
+        {
+            cameraPosition -= direction * deltaTime * forwardSpeed;
+        }
+        // Strafe left
+        if (glfwGetKey(windowID, GLFW_KEY_A) == GLFW_PRESS) 
+        {
+            cameraPosition -= cameraRight * deltaTime * strafeSpeed;
+        }
+        // Move backward
+        if (glfwGetKey(windowID, GLFW_KEY_S) == GLFW_PRESS) 
+        {
+            cameraPosition += direction * deltaTime * forwardSpeed;
+        }
+        // Strafe right
+        if (glfwGetKey(windowID, GLFW_KEY_D) == GLFW_PRESS) 
+        {
+            cameraPosition += cameraRight * deltaTime * strafeSpeed;
+        }
+
+        //// angles
+        //if (glfwGetKey(windowID, GLFW_KEY_UP) == GLFW_PRESS)
+        //{
+        //    pitch += 1.0;
+        //    direction.x -= glm::cos(glm::radians(pitch)) * glm::cos(glm::radians(yaw));
+        //    direction.y -= glm::sin(glm::radians(pitch));
+        //    direction.z -= glm::cos(glm::radians(pitch)) * glm::sin(glm::radians(yaw));
+        //}
+        //// Strafe left
+        //if (glfwGetKey(windowID, GLFW_KEY_LEFT) == GLFW_PRESS)
+        //{
+        //    
+        //}
+        //// Move backward
+        //if (glfwGetKey(windowID, GLFW_KEY_DOWN) == GLFW_PRESS)
+        //{
+        //    pitch -= 1.0;
+        //    direction.x += glm::cos(glm::radians(pitch)) * glm::cos(glm::radians(yaw));
+        //    direction.y += glm::sin(glm::radians(pitch));
+        //    direction.z += glm::cos(glm::radians(pitch)) * glm::sin(glm::radians(yaw));
+        //}
+        //// Strafe right
+        //if (glfwGetKey(windowID, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        //{
+        //    
+        //}
+
+        lastXpos = currXpos;
+        lastYpos = currYpos;
+        glfwGetCursorPos(windowID, &currXpos, &currYpos);
+        yaw += sens * deltaTime * float(currXpos - lastXpos);
+        pitch += sens * deltaTime * float(currYpos - lastYpos);
+
+
+        glm::vec3 moveDirection(
+            cos(pitch) * sin(yaw),
+            sin(pitch),      
+            cos(pitch) * cos(yaw)
+        );
+
+        glm::vec3 right = glm::vec3(
+            sin(yaw - 3.14f / 2.0f),
+            0,
+            cos(yaw - 3.14f / 2.0f)
+        );
+
+        glm::vec3 newUp = glm::cross(right, moveDirection);
+
+        // Move forward
+        if (glfwGetKey(windowID, GLFW_KEY_UP) == GLFW_PRESS) {
+            direction += moveDirection * deltaTime * 5.0f;
+        }
+        // Move backward
+        if (glfwGetKey(windowID, GLFW_KEY_DOWN) == GLFW_PRESS) {
+            direction -= moveDirection * deltaTime * 5.0f;
+        }
+        // Strafe right
+        if (glfwGetKey(windowID, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+            direction += right * deltaTime * 5.0f;
+        }
+        // Strafe left
+        if (glfwGetKey(windowID, GLFW_KEY_LEFT) == GLFW_PRESS) {
+            direction -= right * deltaTime * 5.0f;
+        }
+
+        // mouse movement
+        //if (glfwGetMouseButton(windowID, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+        //{
+        //    // consider infinite dragging (unrestricted by window border) by getting/setting position later
+        //    
+        //    deltaX = currXpos - lastXpos;
+        //    deltaY = currYpos - lastYpos;
+        //    
+        //    yaw -= deltaX * sens;
+        //    pitch += deltaY * sens;
+
+        //    direction.x += glm::cos(yaw);
+        //    direction.z -= glm::sin(yaw);
+        //    direction.y += glm::sin(pitch);
+
+        //}
+        
+
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
@@ -101,12 +242,12 @@ int main()
         glDrawArrays(GL_LINES, 0, 2 * 1);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window.GetID());
+        glfwSwapBuffers(windowID);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
 
-    glfwTerminate();
+    window.~Window();
     return 0;
 }
