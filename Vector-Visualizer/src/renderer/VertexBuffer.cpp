35269977fe7491c417e7bf15ad3a9ff079549bfd
Jason Fong <jasonlmfong@gmail.com>
2023-03-28 18:21:09 -0400

    added dynamic and static mode

--- Vector-Visualizer/src/renderer/VertexBuffer.cpp
+++ Vector-Visualizer/src/renderer/VertexBuffer.cpp
@@ -3,11 +3,21 @@
 #include <iostream>
 #include "glad/glad.h"
 
-VertexBuffer::VertexBuffer(const void *data, unsigned int size)
+VertexBuffer::VertexBuffer(const void *data, unsigned int size, MODE mode)
 {
 	glGenBuffers(1, &m_ID);
 	glBindBuffer(GL_ARRAY_BUFFER, m_ID);
-	glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+	if (mode == MODE::STATIC) {
+		glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+	}
+	else if (mode == MODE::DYNAMIC) {
+		glBufferData(GL_ARRAY_BUFFER, size, data, GL_DYNAMIC_DRAW);
+	}
+	else {
+		glDeleteBuffers(1, &m_ID);
+		std::cout << "Buffer mode error." << std::endl;
+	}
+	
 
 	int bufferSize;
 	glGetBufferParameteriv(GL_ARRAY_BUFFER, GL_BUFFER_SIZE, &bufferSize);
--- Vector-Visualizer/src/renderer/VertexBuffer.h
+++ Vector-Visualizer/src/renderer/VertexBuffer.h
@@ -1,12 +1,16 @@
 #pragma once
 
+enum class MODE {
+	STATIC, DYNAMIC
+};
+
 class VertexBuffer
 {
 private:
 	unsigned int m_ID;
 
 public:
-	VertexBuffer(const void *data, unsigned int size);
+	VertexBuffer(const void *data, unsigned int size, MODE mode);
 	~VertexBuffer();
 
 	void Bind() const;
